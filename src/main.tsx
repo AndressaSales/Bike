c522fbb7852285aaa5bf9fead43f6e8ef245eba5
AndressaSales <andressaccs2001@gmail.com>
2024-12-08 14:50:47 -0300

    instalando o react router dom no projeto

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
+import { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './router/Home.tsx'
+import Register from './router/Register.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/register",
+        element: <Register/ >
+      }
+    ]
+  },
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
